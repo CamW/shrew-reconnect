f23d20d96d819f5694c062aba748ed2600d04fc7
Marcus Bowyer <marcus@tarukino.com>
2018-02-07 17:00:56 -0800

    added new connection verification method

--- ShrewReconnect/Shrew/ShrewConnection.cs
+++ ShrewReconnect/Shrew/ShrewConnection.cs
@@ -12,7 +12,7 @@ namespace com.waldron.shrewReconnect
         private const int MONITOR_INTERVAL_MS = 18000; //18 secs
         private const int RETRY_INTERVAL_MS = 60000; //1 min
         private const int WAIT_INTERVAL_MS = 1000; //1 sec
-        
+
         private const string IKE_DAEMON_NAME = "ShrewSoft IKE Daemon";
         private const string IPSEC_DAEMON_NAME = "ShrewSoft IPSEC Daemon";
 
@@ -21,22 +21,41 @@ namespace com.waldron.shrewReconnect
         private int failedConnectAttempts { get; set; }
         private bool shuttingDown { get; set; }
         private ShrewClientService vpnClient { get; set; }
+        private string networkHost { get; set; }
 
         public ShrewConnection(ShrewCredentials credentials)
         {
             vpnClient = new ShrewClientService(credentials);
+            this.networkHost = GetShrewConfigLine("network-host", credentials.siteConfigPath);
             this.shuttingDown = false;
             this.failedConnectAttempts = 0;
         }
 
-        public void Connect() {
+        public string GetShrewConfigLine(string configLine, string siteConfigPath)
+        {
+            string[] lines = System.IO.File.ReadAllLines($@"{Environment.GetEnvironmentVariable("LOCALAPPDATA")}\Shrew Soft VPN\sites\{siteConfigPath}");
+            Char delimiter = ':';
+            foreach (string line in lines)
+            {
+                String[] settings = line.Split(delimiter);
+                if (settings[1] == configLine)
+                {
+                    return settings[2];
+                }
+            }
+            throw new Exception();
+        }
+
+        public void Connect()
+        {
             this.shuttingDown = false;
             this.primaryThread = new Thread(new ThreadStart(ConnectProcess));
             this.primaryThread.IsBackground = true;
             this.primaryThread.Start();
         }
 
-        private void ConnectProcess() {
+        private void ConnectProcess()
+        {
             try
             {
                 this.failedConnectAttempts = 0;
@@ -59,14 +78,16 @@ namespace com.waldron.shrewReconnect
                 ShrewNotifier.SetStatus(ShrewConnectionStatus.Connected);
                 ShrewNotifier.Log("Connection established.", ShrewConnectionStatus.Connected);
                 ShrewNotifier.Log("------------------------------------------------", ShrewConnectionStatus.Pending);
-            }catch (Exception exc)
+            }
+            catch (Exception exc)
             {
                 ShrewNotifier.SetStatus(ShrewConnectionStatus.Disconnected);
                 ShrewNotifier.Log(string.Format("Error while trying to connect: {0}\r\n\r\n {1}", exc.Message, exc.StackTrace), ShrewConnectionStatus.Disconnected);
             }
         }
 
-        private bool ConnectAttempt() {
+        private bool ConnectAttempt()
+        {
             CleanUp();
             vpnClient.ExecuteClient();
             long startTimeTicks = DateTime.UtcNow.Ticks;
@@ -93,7 +114,8 @@ namespace com.waldron.shrewReconnect
             this.monitorTimer = new Timer(RunMonitorProcess, null, MONITOR_INTERVAL_MS, MONITOR_INTERVAL_MS);
         }
 
-        private void RunMonitorProcess(object state){
+        private void RunMonitorProcess(object state)
+        {
             lock (this)
             {
                 if (!VerifyConnected())
@@ -108,6 +130,22 @@ namespace com.waldron.shrewReconnect
         private bool VerifyConnected()
         {
             NetworkInterface[] netInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+
+            // try to verify connection using domain method
+            foreach (NetworkInterface adapter in netInterfaces)
+            {
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                if (adapterProperties.DnsSuffix == this.networkHost)
+                {
+                    return true;
+                }
+                if (this.networkHost.Contains(adapterProperties.DnsSuffix) && (adapterProperties.DnsSuffix != ""))
+                {
+                    return true;
+                }
+            }
+
             NetworkInterface vpnInterface = netInterfaces.FirstOrDefault(x => x.Description.Equals("Shrew Soft Virtual Adapter"));
             if (vpnInterface != null)
             {
@@ -116,7 +154,8 @@ namespace com.waldron.shrewReconnect
             return false;
         }
 
-        private void CleanUp() {
+        private void CleanUp()
+        {
             ShrewNotifier.Log("Destroying any pre-existing vpn clients.", ShrewConnectionStatus.Pending);
             if (this.monitorTimer != null)
             {
@@ -133,7 +172,8 @@ namespace com.waldron.shrewReconnect
             this.CleanUp();
         }
 
-        public void ToggleShrewVisible() {
+        public void ToggleShrewVisible()
+        {
             vpnClient.ToggleShrewVisible();
         }
 
